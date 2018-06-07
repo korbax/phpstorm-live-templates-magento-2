f649153516c344e64ba1a7cbe8f1849c1428af68
Aleksandr Drobot <alexander.drobot@fortyfour.com>
2018-06-07 18:03:33 +0300

    Create InstallData.php

---
+++ M2/InstallData.php
@@ -0,0 +1,27 @@
+<?php
+namespace $NAMESPACE$\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * Installs DB schema for a module
+     * 
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     */
+    public function install(
+        ModuleDataSetupInterface $setup,
+        ModuleContextInterface $context 
+    )
+    {
+        $setup->startSetup();
+
+
+        $setup->endSetup();
+    }
+
+}
