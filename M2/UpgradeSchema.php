0a9bda31692e47607efaad93a00ca6cf83feadd1
Aleksandr Drobot <alexander.drobot@fortyfour.com>
2018-06-08 16:52:27 +0300

    Create UpgradeSchema.php

---
+++ M2/UpgradeSchema.php
@@ -0,0 +1,20 @@
+namespace $NAMESPACE$\Setup;
+
+use Magento\Framework\DB\Ddl\Table;
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+        
+        if (version_compare($context->getVersion(), '1.0.1', '<')) {
+
+        }
+        
+        $setup->endSetup();
+    }
+}
