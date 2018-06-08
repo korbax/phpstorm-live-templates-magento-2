2a7c963d10699ab38e37e88a064df4f28126d3a5
Aleksandr Drobot <alexander.drobot@fortyfour.com>
2018-06-08 16:53:39 +0300

    Create UpgradeData.php

---
+++ M2/UpgradeData.php
@@ -0,0 +1,20 @@
+<?php
+namespace $NAMESPACE$\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        if (version_compare($context->getVersion(), '1.0.1', '<')) {
+            
+        }
+
+        $setup->endSetup();
+    }
+}
